a3481c369eadf3ba2e6822b3c03eefbc35a8417c
Jelle Langedijk <60575963+JelleLa@users.noreply.github.com>
2020-07-02 20:01:25 +0200

    Add files via upload

---
+++ python_math.py
@@ -0,0 +1,9 @@
+#! usr/bin/python3
+
+# IMPORTS
+from math import pi, exp, log, factorial
+from numpy.lib.scimath import *      
+from sympy import Symbol, symbols, diff, integrate, Rational, lambdify, solve, latex
+
+
+# CODE
