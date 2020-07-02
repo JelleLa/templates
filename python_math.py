ea0d542285e817cd6c694c27dd4a4e14ec42c1b7
Jelle Langedijk <60575963+JelleLa@users.noreply.github.com>
2020-07-02 22:21:25 +0200

    Delete python_math.py

--- python_math.py
+++
@@ -1,9 +0,0 @@
-#! usr/bin/python3
-
-# IMPORTS
-from math import pi, exp, log, factorial
-from numpy.lib.scimath import *      
-from sympy import Symbol, symbols, diff, integrate, Rational, lambdify, solve, latex
-
-
-# CODE
