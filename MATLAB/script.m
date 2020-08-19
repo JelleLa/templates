f9de74664b8c54a2dd2ed6634aa38c623e0e4f3d
Jelle Langedijk <60575963+JelleLa@users.noreply.github.com>
2020-08-19 20:22:21 +0200

    Create script.m

---
+++ MATLAB/script.m
@@ -0,0 +1,12 @@
+%==============================================================================
+% NAME OF CODE / WRITER / INSTITUTE
+% V1.0 (C) 2020 Jelle Langedijk, all rights reserved
+% github.com/JelleLa/templates/MATLAB/script.m
+%==============================================================================
+
+%% PREREQUISITIES
+clear all; close all; clear vars;
+
+%% SECTION HEADER
+
+% COMMENT
