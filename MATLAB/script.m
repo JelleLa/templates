da863e3b1c008ee77ca7d18b5286ad8d2b5402f5
Jelle Langedijk <60575963+JelleLa@users.noreply.github.com>
2020-08-19 20:23:26 +0200

    Update script.m

--- MATLAB/script.m
+++ MATLAB/script.m
@@ -1,7 +1,7 @@
 %==============================================================================
 % NAME OF CODE / WRITER / INSTITUTE
 % V1.0 (C) 2020 Jelle Langedijk, all rights reserved
-% github.com/JelleLa/templates/MATLAB/script.m
+% https://github.com/JelleLa/templates/tree/master/MATLAB
 %==============================================================================
 
 %% PREREQUISITIES
