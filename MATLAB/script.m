8b63427f32bdd00b6bf71dc26697646479b73f04
Jelle Langedijk <60575963+JelleLa@users.noreply.github.com>
2020-09-06 13:29:38 +0200

    Update script.m

--- MATLAB/script.m
+++ MATLAB/script.m
@@ -1,11 +1,11 @@
 %==============================================================================
 % NAME OF CODE / WRITER / INSTITUTE
-% V1.0 (C) 2020 Jelle Langedijk, all rights reserved
+% V1.1 (C) 2020 Jelle Langedijk, all rights reserved
 % https://github.com/JelleLa/templates/tree/master/MATLAB
 %==============================================================================
 
 %% PREREQUISITIES
-clear all; close all; clear vars;
+clear all; close all; clear vars; clc;
 
 %% SECTION HEADER
 
